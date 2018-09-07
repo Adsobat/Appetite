102fd0dd9cdfef73bc8e141925cf92156138ab30
js274 <js274@hdm-stuttgart.de>
2018-09-07 16:05:12 +0200

    Started working on the base classes for the cards and added a dummy Card for testing

---
+++ Appetite/Assets/Scripts/BaseClasses/CardBehaviourBase.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CardBehaviourBase : MonoBehaviour {
+	// costs for gameplay
+	public int cookCost;
+	public float calories;
+	public float thirst;
+	public float costMonney = 13;
+	public TextMesh costText;
+
+	void Update () {
+		UpdateUI();
+		// also using mouse down for testing
+		if (Input.touchCount > 0 || Input.GetMouseButtonDown(0)) {			
+			print("Tougcht");
+			//just to be sure
+			costMonney = 0;
+			
+		}
+
+	}
+	public void OnEaten(){
+
+	}
+	public void OnPlayed(){
+
+	}
+	public void OnRemoved(){
+
+	}
+	public void OnEnhanced(){
+
+	}
+	public void OnDrawn(){
+
+	}
+	private void UpdateUI(){
+		if(costText !=null){
+			costText.text = costMonney + " €";
+		}
+
+	}
+
+}
